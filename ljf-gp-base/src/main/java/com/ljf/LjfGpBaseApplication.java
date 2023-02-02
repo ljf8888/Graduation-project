f98dc4f602b837f8a57bcfb7a760b0edc60436ef
Administrator <2813119976@qq.com>
2023-02-02 12:27:56 +0800

    上传base模块和parent模块

---
+++ ljf-gp-base/src/main/java/com/ljf/LjfGpBaseApplication.java
@@ -0,0 +1,13 @@
+package com.ljf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjfGpBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjfGpBaseApplication.class, args);
+    }
+
+}
