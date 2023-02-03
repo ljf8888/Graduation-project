7bf4e5e4d15ea11a0fc8c1ad351ca34ca4792468
Administrator <2813119976@qq.com>
2023-02-03 13:32:19 +0800

    上传content模块框架

---
+++ ljf-gp-content/ljf-gp-content-service/src/main/java/com/example/ljfapicontentservice/LjfApiContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ljfapicontentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjfApiContentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjfApiContentServiceApplication.class, args);
+    }
+
+}
---
+++ ljf-gp-content/ljf-gp-content-service/src/test/java/com/example/ljfapicontentservice/LjfApiContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ljfapicontentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjfApiContentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
