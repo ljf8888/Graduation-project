d1a21ce970a17d2e780049c41c8c09ab1d1810c8
Administrator <2813119976@qq.com>
2023-02-08 16:08:49 +0800

    上传编辑内容接口

---
+++ ljf-gp-content/ljf-gp-content-service/src/main/java/com/ljf/service/CourseMarketService.java
@@ -0,0 +1,14 @@
+package com.ljf.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ljf.po.CourseMarket;
+
+/**
+ * @Classname CourseMarketService
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2023/2/8 15:21
+ * @Created by 李炯飞
+ */
+public interface CourseMarketService extends IService<CourseMarket> {
+}
